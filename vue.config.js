2c80921b9e0749c9a315a46626ce4f7ef7950e92
Yevhenii Rizchenko <veuzqpvo@email.com>
2021-08-14 03:19:06 +0300

    deploy scripts

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/med/" : "/",
-  // css: {
-  //   loaderOptions: {
-  //     scss: {
-  //       additionalData: `@import "@/assets/scss/main.scss";`,
-  //     },
-  //   },
-  // },
 };
